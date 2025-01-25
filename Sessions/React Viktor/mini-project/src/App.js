a0153517180feb19f926446dd4c29b56ef2156d6
ShuajbBunjaku <143562145+ShuajbBunjaku@users.noreply.github.com>
2025-01-25 15:27:08 +0100

    a

--- Sessions/React Viktor/mini-project/src/App.js
+++ Sessions/React Viktor/mini-project/src/App.js
@@ -1,28 +1,27 @@
-import './App.css';
-import React, { useEffect, useState } from 'react'
-import Quiz from './components/Quiz/Quiz';
-import { mockFetch } from './utils/utils';
-import { useParams } from 'react-router-dom';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Quiz from "./components/Quiz/Quiz";
+import { mockFetch } from "./utils/utils";
 
 function App() {
-    
-    const {quizId, questionId} = useParams()
+  const [questions, setQuestions] = useState([]);
+  const [quiz, setQuiz] = useState(1);
 
-    const [questions, setQuestions] = useState([]);
-    const [quiz, setQuiz] = useState(parseInt(quizId));
-    
-    
-    useEffect(() => {
-        mockFetch(`quiz/${quiz}`)
-        .then(res => res.json())
-        .then(res => setQuestions(res));
-    }, [quiz])
+  useEffect(() => {
+    mockFetch(`quiz/${quiz}`)
+      .then((res) => res.json())
+      .then((res) => setQuestions(res));
+  }, [quiz]);
 
-    return (
-        <div className='Center-Content'>
-            <Quiz quiz={quiz} questions={questions} ></Quiz>
-        </div>
-    )
+  return (
+    <div className="Center-Content">
+      <Quiz
+        quiz={quiz}
+        questions={questions}
+        onNext={() => setQuiz((quiz) => quiz + 1)}
+      ></Quiz>
+    </div>
+  );
 }
 
-export default App
+export default App;
