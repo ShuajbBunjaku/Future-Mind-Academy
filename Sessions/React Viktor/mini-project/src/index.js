ed1b096618bd27dc1ddf3fd1e0bbeeba3aa4f38a
ShuajbBunjaku <143562145+ShuajbBunjaku@users.noreply.github.com>
2025-01-25 15:29:55 +0100

    Update index.js

--- Sessions/React Viktor/mini-project/src/index.js
+++ Sessions/React Viktor/mini-project/src/index.js
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App></App>);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path="/quiz/quizId/question/questionId"
+        element={<App></App>}
+      ></Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
